32db76a5b7968a37f019be5a67a013c56f456855
chenweiheng <849996461@qq.com>
2021-01-23 17:25:21 +0800

    add flux

---
+++ algorithm/src/main/java/chen/UnionSet.java
@@ -0,0 +1,51 @@
+package chen;
+
+/**
+ * @see https://oi-wiki.org/ds/dsu/
+ * 并查集  , 用于集合的合并
+ *
+ */
+public class UnionSet  {
+
+    int[] fa = new int[1000];
+    int[] sizes = new int[1000];
+
+
+    public UnionSet(int n){
+        fa = new int[n];
+        for (int i = 0; i < n; i++) {
+            fa[i] = i;
+        }
+        sizes = new int[n];
+    }
+
+    /**
+     * 获取该元素的祖先
+     * @return
+     */
+    public int find(int x){
+        if (fa[x] != x) {
+            fa[x] = find(x);
+        }
+        return fa[x];
+    }
+
+    /**
+     * 合并两个家族
+     */
+    public void union(int x, int y) {
+        //同祖先 , 不需要合并
+        int xx = fa[x], yy = fa[y];
+        if (xx == yy) {
+            return;
+        }
+        //yy的size比较小
+        if (sizes[xx] <= sizes[yy]) {
+            int t = yy;
+            yy = xx;
+            xx = t;
+        }
+        fa[yy] = xx;
+        sizes[xx] += sizes[yy];
+    }
+}
