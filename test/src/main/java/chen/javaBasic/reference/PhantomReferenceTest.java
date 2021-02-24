4dac54d9e6bbd066601258550d7161d222696a4d
chenwh <chenwh@eastcom-sw.com>
2021-02-24 18:32:05 +0800

    add:

--- test/src/main/java/chen/javaBasic/reference/PhantomReferenceTest.java
+++ test/src/main/java/chen/javaBasic/reference/PhantomReferenceTest.java
@@ -2,25 +2,50 @@ package chen.javaBasic.reference;
 
 import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * 虚引用 , gc不会回收 , 但会收到通知
+ */
 public class PhantomReferenceTest {
+
+
    public static void main(String[] args)
       throws Exception {
-      // 创建一个字符串对象
-      String str = new String("疯狂Java讲义");
-      // 创建一个引用队列
+      byte[] bytes = new byte[1024 * 1024 * 128];
       ReferenceQueue rq = new ReferenceQueue();
-      // 创建一个虚引用，让此虚引用引用到"疯狂Java讲义"字符串
-      PhantomReference pr = new PhantomReference (str , rq);
-      // 切断str引用和"疯狂Java讲义"字符串之间的引用
-      str = null;
+      PhantomReference pr = new PhantomReference (bytes , rq);
+      bytes = null;
       // 取出虚引用所引用的对象，并不能通过虚引用获取被引用的对象，所以此处输出null
-      System.out.println(pr.get());  //①
+      System.out.println(pr.get());
       // 强制垃圾回收
+      TimeUnit.SECONDS.sleep(2);
+      System.out.println("开始垃圾回收");
       System.gc();
       System.runFinalization();
       // 垃圾回收之后，虚引用将被放入引用队列中
+      System.out.println("垃圾回收结束");
+      TimeUnit.SECONDS.sleep(2);
+
       // 取出引用队列中最先进入队列中的引用与pr进行比较
-      System.out.println(rq.poll() == pr);   //②
+      System.out.println(rq.poll() == pr);
+
+      TimeUnit.SECONDS.sleep(4);
+
+      pr.clear();
+      System.out.println("开始垃圾回收2");
+      System.gc();
+      System.runFinalization();
+      // 垃圾回收之后，虚引用将被放入引用队列中
+      System.out.println("垃圾回收结束2");
+
+      TimeUnit.SECONDS.sleep(10);
+
+
+
+
+
+
    }
 }
