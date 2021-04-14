baf8ede7d1a7709c329cd3d6751c42881fb30076
chenwh <chenwh@eastcom-sw.com>
2021-04-14 15:36:27 +0800

    add:

--- algorithm/src/main/java/chen/Test.java
+++ algorithm/src/main/java/chen/Test.java
@@ -9,8 +9,4 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 
 public class Test {
-    public static void main(String[] args){
-        AtomicInteger i = new AtomicInteger();
-        i.incrementAndGet()
-    }
 }
