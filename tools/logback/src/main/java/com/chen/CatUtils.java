4ca2f2b8467e1f031ec62b6494884b6dc29051d7
chenwh <chenwh@eastcom-sw.com>
2021-05-06 10:43:17 +0800

    add:

---
+++ tools/logback/src/main/java/com/chen/CatUtils.java
@@ -0,0 +1,28 @@
+package com.chen;
+
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Transaction;
+import com.mchange.v2.log.LogUtils;
+import jdk.nashorn.api.scripting.ScriptUtils;
+
+import javax.script.ScriptContext;
+import java.util.Arrays;
+
+class CatUtils {
+
+
+    public static void main(String[] args) {
+//        byte[] bytes = new byte[]{36, 74, 73, 77, 77, 77, 69, 49, 48, 50, 66, 90, 88, 45, 67, 79, 77, 66, 79, 95, 77, 77, 69, 71, 78, 71, 80, 95, 83, 71, 83, 78, 95, 49, 48, 50, 62, 83, 72, 79, 87, 32, 69, 78, 79, 68, 69, 66, 32, 73, 78, 70, 79, 58, 77, 67, 67, 61, 52, 54, 48, 44, 77, 78, 67, 61, 48, 48, 44, 69, 78, 79, 68, 69, 66, 73, 68, 61, 56, 50, 57, 56, 50, 53, 13, 10, 46, 46, 46, 46, 46, 46, 124
+//        };
+//
+//        System.out.println(new String(bytes));
+//        byte b = 10;
+//        System.out.println(b == '\n');
+        System.out.println(Arrays.toString("|".getBytes()));
+
+    }
+
+
+
+
+}
--- tools/logback/src/main/java/com/chen/Test.java
+++ tools/logback/src/main/java/com/chen/Test.java
@@ -1,7 +1,9 @@
 package com.chen;
 
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Event;
+import com.dianping.cat.message.Transaction;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 
 /**
  * @author chenwh
@@ -9,7 +11,23 @@ import org.slf4j.MDC;
  */
 @Slf4j
 public class Test {
-    public static void main(String[] args) {
-        log.info("1231234");
+    public static void main(String[] args) throws Exception {
+        CatContextImpl catContext = new CatContextImpl();
+        catContext.addProperty(Cat.Context.ROOT, "123");
+        catContext.addProperty(Cat.Context.PARENT, "234");
+        catContext.addProperty(Cat.Context.CHILD, "345");
+        Cat.logRemoteCallServer(catContext);
+        Transaction t = Cat.newTransaction("safsfasd", "demo");
+        Cat.logMetric("fuck");
+        Cat.logMetricForCount("fuck");
+        Cat.logMetricForCount("fuck");
+        Cat.logMetricForCount("fuck", 5);
+
+        Cat.logEvent("dddd", "ccccc", Event.SUCCESS, "ip=${serverIp}");
+        t.complete();
+        Cat.logEvent("aaaaaaa", "hfkjahsfjkahds", Event.SUCCESS, "ip=${serverIp}");
+        System.out.println("end");
+
+        Thread.sleep(100000000); // 此处 sleep 一会, 就能保证 CAT 异步消息发送
     }
 }
