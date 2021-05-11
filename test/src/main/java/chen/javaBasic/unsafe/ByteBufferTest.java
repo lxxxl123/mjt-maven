fb34bce5c8a1e6508064ac70a247b1a10819d1d1
chenweiheng <849996461@qq.com>
2021-05-11 19:40:18 +0800

    add .

--- test/src/main/java/chen/javaBasic/unsafe/ByteBufferTest.java
+++ test/src/main/java/chen/javaBasic/unsafe/ByteBufferTest.java
@@ -8,9 +8,10 @@ import java.util.concurrent.TimeUnit;
 public class ByteBufferTest {
 
     public static void main(String[] args) throws Exception {
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("开始分配100m内存");
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(100 * 1024 * 1024);
-        TimeUnit.SECONDS.sleep(10000);
+        while (true) {
+            TimeUnit.SECONDS.sleep(5);
+            System.out.println("开始分配100m内存");
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(100 * 1024 * 1024);
+        }
     }
 }
