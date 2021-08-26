8ddab79e3eacf5166729c887a126bd58f53c9721
849996461 <849996461@qq.com>
2021-08-27 02:29:56 +0800

    add:

--- netty-proxy/src/main/java/com/chen/netty/proxy/test/server/http/handler/ProxyRequestHandler.java
+++ netty-proxy/src/main/java/com/chen/netty/proxy/test/server/http/handler/ProxyRequestHandler.java
@@ -1,7 +1,7 @@
 package com.chen.netty.proxy.test.server.http.handler;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -9,14 +9,10 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
-import reactor.netty.http.client.HttpClient;
+import reactor.netty.Connection;
 import reactor.netty.tcp.TcpClient;
 
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author chenwh
@@ -28,20 +24,19 @@ public class ProxyRequestHandler extends ChannelInboundHandlerAdapter {
     public static NioEventLoopGroup group = new NioEventLoopGroup(300);
 
     @Override
-    public  void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         log.info("receive =[{}] , type=[{}]", msg, msg.getClass());
         if (!(msg instanceof FullHttpRequest)) {
             return;
         }
         FullHttpRequest res = ((FullHttpRequest) msg);
         String[] host_port = res.headers().get("host").split(":");
+        ctx.pipeline().remove("httpCodec");
+        ctx.pipeline().remove("httpObject");
         if (HttpMethod.CONNECT.equals(res.method())) {
             HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
             ctx.writeAndFlush(response);
-            ctx.pipeline().remove("httpCodec");
-            ctx.pipeline().remove("httpObject");
-
-        }else{
+        } else {
             try {
                 String remoteHost = host_port[0];
                 int remotePort = Integer.parseInt(host_port[1]);
@@ -53,38 +48,35 @@ public class ProxyRequestHandler extends ChannelInboundHandlerAdapter {
     }
 
 
-    public void sendWithHttpClient(String ip ,int port , ChannelHandlerContext ctx, FullHttpRequest msg){
+    public void sendWithHttpClient(String ip, int port, ChannelHandlerContext ctx, FullHttpRequest msg) {
         EmbeddedChannel ch = new EmbeddedChannel(new HttpRequestEncoder());
         ch.writeOutbound(msg);
         ByteBuf byteBuf = ch.readOutbound();
-        ctx.pipeline().remove("httpCodec");
+//        ctx.pipeline().remove("httpCodec");
+        AtomicReference<Connection> conn = new AtomicReference<>();
 
-        TcpClient
-                .newConnection()
+        Connection connection = TcpClient.create()
                 .host(ip).port(port)
-                .connect()
-                .flatMap(connection ->
-                        connection.outbound().sendObject(byteBuf)
-                                .then(connection.inbound()
-                                        .receiveObject().next()
-                                        .flatMap(receive -> {
-                                            ChannelFuture channelFuture = ctx.writeAndFlush(Unpooled.wrappedBuffer(((ByteBuf) receive).copy()));
-                                            channelFuture.addListener(li -> {
-                                                if (li.isSuccess()) {
-                                                    ctx.close();
-                                                } else {
-                                                    log.error("123");
-                                                }
-                                            });
-                                            return Mono.empty();
-                                        }))
-                                .then()
-                ).then()
-                .block();
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000)
+                .handle((in, out) -> out.sendObject(byteBuf).then(
+                        in.receive().flatMap(receive -> {
+                            receive.retain();
+                            ctx.writeAndFlush(receive).addListener(li -> {
+                                if (li.isSuccess()) {
+                                    ctx.close();
+                                } else {
+                                    log.error("write fail");
+                                }
+                                conn.get().dispose();
+                            });
+                            return Mono.empty();
+                        }))
+                ).connectNow();
+        conn.set(connection);
 
     }
 
-    public void sendHttpWithNetty(String ip ,int port , ChannelHandlerContext ctx, Object msg){
+    public void sendHttpWithNetty(String ip, int port, ChannelHandlerContext ctx, Object msg) {
         String remoteHost = ip;
         int remotePort = port;
 
