cf225f92d04488dccba05c222c10751d985f0c82
chenweiheng <849996461@qq.com>
2021-08-31 00:49:10 +0800

    add .

--- netty-proxy/src/main/java/com/chen/netty/proxy/test/Application.java
+++ netty-proxy/src/main/java/com/chen/netty/proxy/test/Application.java
@@ -16,7 +16,7 @@ public class Application {
     public static void main(String[] args) throws Exception {
         new SimHttpServer().create();
         new HttpProxyServer().create();
-        TimeUnit.DAYS.sleep(1);
+        TimeUnit.DAYS.sleep(2);
 
     }
 }
