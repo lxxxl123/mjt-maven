b534f97fbf8a0581a0bc96671ee730f048983b78
849996461 <849996461@qq.com>
2021-09-18 16:03:52 +0800

    fix add https

--- rsocket-test/src/main/java/com/chen/rsocket/test/Main.java
+++ rsocket-test/src/main/java/com/chen/rsocket/test/Main.java
@@ -1,5 +1,9 @@
 package com.chen.rsocket.test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author chenwh
  * @date 2021/9/16
@@ -8,6 +12,7 @@ package com.chen.rsocket.test;
 public class Main {
 
     public static void main(String[] args) {
-
+        List<Integer> collect = Stream.of(1, 2, 3).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
