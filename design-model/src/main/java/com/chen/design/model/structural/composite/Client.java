555273b237e95319e006d7c689a9c5518faba66e
849996461 <849996461@qq.com>
2021-09-26 15:11:37 +0800

    fix dm

---
+++ design-model/src/main/java/com/chen/design/model/structural/composite/Client.java
@@ -0,0 +1,17 @@
+package com.chen.design.model.structural.composite;
+
+/**
+ * @author chenwh
+ * @date 2021/9/26
+ */
+
+public class Client {
+
+    /**
+     * Name: 组合模式
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ design-model/src/main/java/com/chen/design/model/structural/facade/Client.java
@@ -0,0 +1,17 @@
+package com.chen.design.model.structural.facade;
+
+/**
+ * @author chenwh
+ * @date 2021/9/26
+ */
+
+public class Client {
+    /**
+     * Name: 外观模式
+     * Impl: 只暴露一个最少方法 , 对外提供多个功能
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
