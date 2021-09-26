a9ce9c5f9b1d14afaf01567aba2c9f35ea485e8d
849996461 <849996461@qq.com>
2021-09-26 15:02:55 +0800

    fix dm

--- design-model/src/main/java/com/chen/design/model/structural/flyweight/Client.java
+++ design-model/src/main/java/com/chen/design/model/structural/flyweight/Client.java
@@ -1,5 +1,11 @@
 package com.chen.design.model.structural.flyweight;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author chenwh
  * @date 2021/9/26
@@ -7,11 +13,27 @@ package com.chen.design.model.structural.flyweight;
 
 public class Client {
 
+    private static Map<String, String> map = new HashMap<>();
+
+    public static String getInstance(String type) {
+        String s = map.get(type);
+        if (s == null) {
+            s = new String("instance" + type);
+            map.put(type, s);
+        }
+        return s;
+
+    }
+
     /**
      * Name: 享元模式
      * 目的: 减少内存的使用
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        List<String> s = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            s.add(getInstance("123"));
+        }
+        TimeUnit.SECONDS.sleep(10000);
     }
 }
---
+++ design-model/src/main/java/com/chen/design/model/structural/proxy/Client.java
@@ -0,0 +1,17 @@
+package com.chen.design.model.structural.proxy;
+
+/**
+ * @author chenwh
+ * @date 2021/9/26
+ */
+
+public class Client {
+
+    /**
+     * Name: 代理模式
+     * 作用: 不修改代码 , 增加功能
+     */
+    public static void main(String[] args) {
+
+    }
+}
