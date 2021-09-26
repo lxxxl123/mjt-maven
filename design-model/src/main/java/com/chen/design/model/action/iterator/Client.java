e782ca58d15a0dd18ffbd0d0b15994c6367759f0
849996461 <849996461@qq.com>
2021-09-26 10:56:26 +0800

    fix add https

---
+++ design-model/src/main/java/com/chen/design/model/action/iterator/Client.java
@@ -0,0 +1,17 @@
+package com.chen.design.model.action.iterator;
+
+/**
+ * @author chenwh
+ * @date 2021/9/26
+ */
+
+public class Client {
+
+    /**
+     * Name: 迭代器模式
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ design-model/src/main/java/com/chen/design/model/action/mediator/Client.java
@@ -0,0 +1,20 @@
+package com.chen.design.model.action.mediator;
+
+import java.util.Timer;
+
+/**
+ * @author chenwh
+ * @date 2021/9/26
+ */
+
+public class Client {
+
+    /**
+     * Name: 中介者模式
+     * Example: java.util.Timer#sched
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
