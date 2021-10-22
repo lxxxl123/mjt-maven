613d72c0f242b12d9c4f7f2e777417737a4907f5
849996461 <849996461@qq.com>
2021-10-22 18:09:03 +0800

    fix dm

---
+++ fast-jar/src/main/java/com/chen/fast/jar/Main.java
@@ -0,0 +1,14 @@
+package com.chen.fast.jar;
+
+/**
+ * @author chenwh
+ * @date 2021/10/22
+ */
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(HostUtil.getAllIp());
+    }
+}
