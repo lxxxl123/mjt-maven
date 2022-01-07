58b85155a553508afa3bff7d2ee5a2df2b3e09d0
849996461 <849996461@qq.com>
2022-01-07 15:31:13 +0800

    add protobuf

---
+++ mysql-driver/src/main/java/com/chen/mysql/MysqlApplication.java
@@ -0,0 +1,17 @@
+package com.chen.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenwh
+ * @date 2022/1/6
+ */
+@SpringBootApplication
+public class MysqlApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlApplication.class);
+    }
+}
