bf999fa2e41ca0459ccd39fe367cde32a3ee8a04
849996461 <849996461@qq.com>
2022-01-13 18:03:54 +0800

    add protobuf

--- spring-stomp/src/main/java/com/chen/ws/config/WsConfig.java
+++ spring-stomp/src/main/java/com/chen/ws/config/WsConfig.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.AbstractHandshakeHandler;
 
 /**
  * @author chenwh
@@ -17,7 +18,9 @@ public class WsConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("test").withSockJS();
+        registry.addEndpoint("test")
+//                .setHandshakeHandler()
+                .withSockJS();
 
     }
 
