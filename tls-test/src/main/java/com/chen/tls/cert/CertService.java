dc6420886251d794a725f6992f95eff424086953
849996461 <849996461@qq.com>
2022-01-17 18:28:59 +0800

    add protobuf

---
+++ tls-test/src/main/java/com/chen/tls/cert/CertService.java
@@ -0,0 +1,13 @@
+package com.chen.tls.cert;
+
+/**
+ * @author chenwh
+ * @date 2022/1/17
+ */
+
+public class CertService {
+
+    public void createCert(){
+
+    }
+}
--- tls-test/src/main/java/com/chen/tls/https/Test1.java
+++ tls-test/src/main/java/com/chen/tls/https/Test1.java
@@ -1,15 +1,10 @@
-package com.chen.netty.proxy.test.server.https.reactor;
+package com.chen.tls.https;
 
-import com.chen.netty.proxy.test.server.Server;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
-import org.springframework.core.io.ClassPathResource;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.server.HttpServer;
 
-import javax.net.ssl.KeyManagerFactory;
-
-import java.io.File;
 import java.io.InputStream;
 
 import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
@@ -20,12 +15,12 @@ import static io.netty.handler.codec.http.HttpHeaderValues.TEXT_PLAIN;
  * @date 2021/9/7
  */
 
-public class ReactorHttpsServer implements Server {
+public class Test1 implements Server {
 
     @Override
     public void create() throws Exception {
         HttpServer server = HttpServer.create()
-                .port(8998)
+                .port(8997)
                 .route(r -> {
                     r.get("/hello", (req, res) -> {
                         return res.header(CONTENT_TYPE, TEXT_PLAIN)
@@ -70,7 +65,7 @@ public class ReactorHttpsServer implements Server {
 
     public static void main(String[] args) {
         try {
-            new ReactorHttpsServer().create();
+            new Test1().create();
         } catch (Exception e) {
             e.printStackTrace();
         }
