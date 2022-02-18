a6d6480c1edb39dc4052f5315f7c65f728069aeb
849996461 <849996461@qq.com>
2022-02-18 14:43:38 +0800

    add tcp ssl connect

--- spring-feign-test/src/main/java/com/chen/feign/test/AdminApp.java
+++ spring-feign-test/src/main/java/com/chen/feign/test/AdminApp.java
@@ -3,6 +3,7 @@ package com.chen.feign.test;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author chenwh
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableAdminServer
 //@EnableDiscoveryClient
+@EnableFeignClients
 public class AdminApp {
     public static void main(String[] args) {
         SpringApplication.run(AdminApp.class,args);
