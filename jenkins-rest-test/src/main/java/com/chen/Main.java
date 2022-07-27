06a0a423c857eebb2552b56f3367db7f0e1990e7
chenwh3 <chenwh3@1234>
2022-07-27 16:47:21 +0800

    add sort algorithm

--- jenkins-rest-test/src/main/java/com/chen/Main.java
+++ jenkins-rest-test/src/main/java/com/chen/Main.java
@@ -1,19 +1,21 @@
 package com.chen;
 
+import com.cdancy.jenkins.rest.JenkinsClient;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
+import org.apache.http.*;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthCache;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
@@ -26,7 +28,10 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author chenwh3
@@ -48,25 +53,48 @@ public class Main {
         authCache.put(host, basicAuth);
         CloseableHttpClient httpClient =
                 HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
-//        HttpGet httpGet = new HttpGet(uri);
-        HttpPost httpPost = new HttpPost(uri);
-        List<BasicNameValuePair> list = new ArrayList<>();
-        list.add(new BasicNameValuePair("Jenkins-Crumb", "fe832c8dc80105fc304f231e1c7a496d1bd39c19e1d7e8fca19605201fb6a865"));
-        list.add(new BasicNameValuePair("Submit", "开始构建"));
-        UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(list);
-        httpPost.setEntity(urlEncodedFormEntity);
-
+        HttpGet httpGet = new HttpGet(uri);
+        httpGet.setHeader("Connection", "keep-alive");
         // Add AuthCache to the execution context
         HttpClientContext localContext = HttpClientContext.create();
         localContext.setAuthCache(authCache);
 
-        HttpResponse response = httpClient.execute(host, httpPost, localContext);
+        HttpResponse r1 = httpClient.execute(host, httpGet, localContext);
+        Header header = r1.getHeaders("Set-Cookie")[0];
+        String node = header.getElements()[0].getValue();
+        String crumb = getCrumb(EntityUtils.toString(r1.getEntity()));
+        System.out.println(crumb);
+        node = "node0h90asjwpgflwthrf22ta212y8219.node0";
+        crumb = "ec9cbdc2bc643e27a418d7dc2ceab09565f9f04ff728bfbbe92a546775def662";
+
+        HttpPost httpPost = new HttpPost("http://192.168.26.2:8080/job/QMS/job/qms-platform-build/build?delay=0sec");
+        httpPost.setHeader("Cookie", "JSESSIONID.0f09aa34=" + node);
+        httpPost.setHeader("Connection", "keep-alive");
+        httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        EntityBuilder
+                .create()
+                .setContentType(ContentType.APPLICATION_FORM_URLENCODED)
+                .setText("name=branch&value=origin%2Ffeature%2Fmarket-complainV1.0.0-front-end&statusCode=303&redirectTo=.&Jenkins-Crumb=" +
+                        crumb +
+                        "&json=%7B%22parameter%22%3A+%7B%22name%22%3A+%22branch%22%2C+%22value%22%3A+%22origin%2Ffeature%2Fmarket-complainV1.0.0-front-end%22%7D%2C+%22statusCode%22%3A+%22303%22%2C+%22redirectTo%22%3A+%22.%22%2C+%22Jenkins-Crumb%22%3A+%22" +
+                        crumb +
+                        "%22%7D&Submit=%E5%BC%80%E5%A7%8B%E6%9E%84%E5%BB%BA").build();
+        HttpEntity entity = httpClient.execute(host, httpPost, localContext).getEntity();
+        System.out.println(EntityUtils.toString(entity));
 
 
-        return EntityUtils.toString(response.getEntity());
+        return null;
     }
 
+    public static String getCrumb(String content){
+        Matcher matcher = CRUMB_PATTERN.matcher(content);
+        matcher.find();
+        return matcher.group(1);
+    }
+
+    public static Pattern CRUMB_PATTERN = Pattern.compile("data-crumb-value=\"(\\w+)\"");
     public static void main(String[] args) throws IOException {
-        System.out.println(scrape("http://192.168.26.2:8080/job/QMS/job/qms-platform-build/build?delay=0sec", "dev", "dev0407@"));
+        System.out.println(scrape("http://192.168.26.2:8080/", "dev", "dev0407@"));
+
     }
 }
