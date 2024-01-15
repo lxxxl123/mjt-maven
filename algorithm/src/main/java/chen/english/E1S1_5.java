5e35c87a9041165c29edfb9a7dc0916984e3714b
chenwh3 <chenwh3@haday.cn>
2024-01-15 18:21:54 +0800

    代码优化

---
+++ algorithm/src/main/java/chen/english/E1S1_5.java
@@ -0,0 +1,46 @@
+package chen.english;
+
+/**
+ * Brenda Feldman
+ * what's that .
+ * A poem I have written for Brenda Feldman
+ * Of course it is .
+ * I put my thoughts into words , and now my words into actions.
+ * I'll give you fifty bucks not to do this .
+ * I'm eleven years old , what am i gonna do with the money
+ * what you gonna do with a sixteen-year-old ?
+ * He's like a bullfighter .
+ * you ever see a bullfight ?
+ * You're in such a bad mood , and I know why ? Because that man thought you're my father.
+ * No - Yes. - No.
+ * When you say "No" like that , it's always "Yes"
+ * Come on , we're in the mall . Let's get you like some younger clothes .
+ * There's a store there .
+ * I don't need any younger clothes .
+ * I don't care what some jackass in pair of ripped jeans thinks about me .
+ * Yes , you shouldn't. you should only care what I think.
+ * I love you , I don't care how old you are .
+ * So , stop being a gloomy goose and stop being so hard on Manny.
+ * The only reason I'm hard on Manny . Is just because I don't wanna he make a fool of himself.
+ * I can smell his hair goo of his from here .
+ * Look , I don't know what gonna happen to him over there . But you're his family now . and that means only one thing . -- You be the wind in his back , not the spit in his face .
+ * What ?
+ * It's something my mon always said . It's gorgeous in Spanish .
+ * Look , he's there .
+ * She has a boyfriend .
+ * Oh , I'm sorry.
+ * I gave her my heart , and she gave me a picture of me as an old-time sheriff .
+ * It's Brave right Jay ? Brave.
+ * You'll be better next time.
+ * Come on , let's get a pretzel .
+ * Uh , execute me sir , we ask all mall walker stay to the right .
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class E1S1_5 {
+}
---
+++ algorithm/src/main/java/chen/english/E1S1_6.java
@@ -0,0 +1,23 @@
+package chen.english;
+
+/**
+ *
+ * Alex , get out! mom !
+ * Alex , leave you sister alone
+ * I was just getting my book , gosh.
+ * I know sweetie , but you need to respect their privacy.
+ * what are they doing up there ?
+ * Nothing . lying on the bed , watching a movie.
+ * okay , okay , I'm making a cake for tonight. You wanna help me with the frosting ?
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class E1S1_6 {
+
+
+}
