4feee962f3da1e32f7666535cdf70f564d96228b
chenweiheng <849996461@qq.com>
2024-01-17 22:59:28 +0800

    add .

--- algorithm/src/main/java/chen/english/E1S1_6.java
+++ algorithm/src/main/java/chen/english/E1S1_6.java
@@ -7,8 +7,12 @@ package chen.english;
  * I was just getting my book , gosh.
  * I know sweetie , but you need to respect their privacy.
  * what are they doing up there ?
- * Nothing . lying on the bed , watching a movie.
+ * Nothing . lying on her bed , watching a movie.
  * okay , okay , I'm making a cake for tonight. You wanna help me with the frosting ?
+ * Sure , you know if Haley get pregnant , would you ever wanna pretend she has mono for a few months
+ * - and then ,like ,tell everyone the baby's yours
+ * What ?
+ * The senior as school was "out sicked" for
  *
  *
  *
