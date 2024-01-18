42fcfe93420434174aa78cec45677f1564d4eab6
chenweiheng <849996461@qq.com>
2024-01-18 22:09:26 +0800

    add .

--- algorithm/src/main/java/chen/english/E1S1_6.java
+++ algorithm/src/main/java/chen/english/E1S1_6.java
@@ -12,7 +12,51 @@ package chen.english;
  * Sure , you know if Haley get pregnant , would you ever wanna pretend she has mono for a few months
  * - and then ,like ,tell everyone the baby's yours
  * What ?
- * The senior as school was "out sicked" for
+ * The senior as school was "out sicked" for fours months
+ * - But Jenna Resnick swears she saw her breast-feeding  at the coin-op car wash.
+ * Buddy , what are you wearing . - Nothing
+ * Uh-uh , no jacket , on hat . how Many pair of underwear do you have on ? - One , ~Six
+ *
+ * First of all , it would be really cool to see Haley that fat .
+ * And how awesome would it be to have a fake little brother who's really my nephew .
+ * Haley is not getting pregnant .
+ * Just saying "if" - I know
+ * And I know you've like to make trouble to you sister , but it's not gonna work this time.
+ * you know why ? Cause you sister's a good girl.
+ * I know . I was just like her when I was ...
+ *
+ * I want you to know , i am not enjoying this .
+ * but this is an important lesson you're learning
+ * So , soak it , keep it
+ * You 're too close , it's gonna hurt
+ * It's supposed to hurt .
+ * And why you're smiling ? I'm what ?
+ *
+ * Oh forget it , I can't do this .
+ * The point is you're scared , I think you've learned your lesson .
+ *
+ * Mom , What are you doing ?
+ * Hey , I was just dropping off some laundry . Is is a bad time ?
+ * Yeah ,
+ * On okay .
+ * can you shot the door please ?
+ * Actually , we're just gonna go ahead and leave that open
+ * Why ?
+ * Because I've seen this little show before .
+ * lying on the bed with the Tall Senior .
+ * One minute , you're just friends , watching Falcon Crest .
+ * And the next you're lying underneath the air hockey table with your bra in your pocket.
+ * Mom!
+ *
+ * You hit my bone . it was an accident .
+ * I thought you were my friend .
+ * I'm your friend.
+ *
+ * Dad,
+ * you have got to talk to mom ,
+ * She's , like , completely freaking out and embarrassing me .
+ * Well , honey , your mom isn't always cool about thing as ..
+ *
  *
  *
  *
